0eb2786bdfbc475b953b9f3920dc702af1a28740
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-02-24 13:20:58 +0530

    Created nested div

--- App.js
+++ App.js
@@ -1,4 +1,21 @@
-// createElement takes 3 argument(element name,attributes,text)
+// createElement takes 3 argument(tag name,attributes like id,[child])
 const heading = React.createElement('h1', {id:"heading"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// root.render(heading);
+
+/**
+ * Create nested div
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>Hello,I'm H1 tag.</h1>
+ *          <h2>Hello,I'm H2 tag.</h2>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement('div',{id:"parent"},
+React.createElement('div',{id:"child"},
+[React.createElement('h1',{},"Hello,I'm H1 tag."),React.createElement('h2',{},"Hello,I'm H2 tag.")]
+))
+
+root.render(parent);
