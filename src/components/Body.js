f24edab5d558b08732cbf452188d79d802ecccfa
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-02-29 01:18:52 +0530

    used react useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,31 @@
 import { resObj } from "../utilities/mockData";
-import RestaurantCard from "./RestaurantCard"
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  //React hook -> It's a utility function
+  //useState is used to store components data as a super variable
+  //[array,updateArray]=useState(default Value)
+  let [ListRestaurant, setListRestaurant] = useState(resObj);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-button"
+          onClick={() => {
+            let filteredList = ListRestaurant.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            console.log(filteredList);
+            setListRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="restaurant-container">
-        {resObj.map((restaurant) => (
+        {ListRestaurant.map((restaurant) => (
           // key is used to make a card unique and shouldn't re-render
           <RestaurantCard key={restaurant.info.id} resName={restaurant} />
         ))}
